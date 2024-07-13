709701176ca4f7908add3abba071b3168b51cfe3
wjkba <63542318+wjkba@users.noreply.github.com>
2024-07-13 12:08:18 +0200

    update

---
+++ nextjs-caching/app/messages/layout.js
@@ -0,0 +1,15 @@
+import { getMessages } from "@/lib/messages";
+
+export default async function MessagesLayout({ children }) {
+  const messages = await getMessages();
+  const totalMessages = messages.length;
+
+  return (
+    <>
+      <h1>Important Messages</h1>
+      <p>{totalMessages} messages found</p>
+      <hr />
+      {children}
+    </>
+  );
+}
---
+++ nextjs-caching/app/messages/page.js
@@ -0,0 +1,17 @@
+import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
+
+export const revalidate = 5;
+
+export default async function MessagesPage() {
+  // const response = await fetch("http://localhost:8080/messages");
+  // const messages = await response.json();
+
+  const messages = await getMessages();
+
+  if (!messages || messages.length === 0) {
+    return <p>No messages found</p>;
+  }
+
+  return <Messages messages={messages} />;
+}
