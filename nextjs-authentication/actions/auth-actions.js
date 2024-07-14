77935db712f1e80dc1c7b35cec876f212b17328b
wjkba <63542318+wjkba@users.noreply.github.com>
2024-07-14 15:20:29 +0200

    update

---
+++ nextjs-authentication/actions/auth-actions.js
@@ -0,0 +1,23 @@
+"use server";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  console.log(email, password);
+  return {};
+}
